1403e49e1d14d21497ea6136a52266961e21a2f9
Basil <basil.taleb@gmail.com>
2019-05-05 21:24:37 +0200

    Initial commit

---
+++ source/RLReplayMan/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using WpfTreeView;
+
+namespace RLReplayMan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var ds = new DirectoryStructureViewModel();
+            this.DataContext = ds;
+        }
+
+        private void FolderView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+            //var files = DirectoryStructureViewModel.SelectedItem.Files;
+            if (DirectoryStructureViewModel.SelectedItem != null)
+            {
+                var selected = (DirectoryItemViewModel)FolderView.SelectedItem;
+                //fileList.ItemsSource = DirectoryStructureViewModel.SelectedItem.Files;
+                fileList.ItemsSource = DirectoryStructure.GetDirectoryFiles(selected.FullPath);
+            }
+        }
+
+    }
+}
