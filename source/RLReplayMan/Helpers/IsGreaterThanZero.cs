705955a341b19e6a72f7651a0b85bedd5fcda062
Basil <basil.taleb@gmail.com>
2019-05-10 16:28:16 +0200

    Add replay directory viewmodel and greater than zero converter.

---
+++ source/RLReplayMan/Helpers/IsGreaterThanZero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace RLReplayMan
+{
+    public class IsGreaterThanZero : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                value = 0;
+            return (int)value > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();//"PresenterConverter.ConvertBack() is not implemented!");
+        }
+        #endregion
+    }
+}
---
+++ source/WpfTreeView/Directory/ViewModels/ReplayDirectoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace WpfTreeView
+{
+    public class ReplayDirectoryViewModel : BaseViewModel
+    {
+        public ObservableCollection<DirectoryItemViewModel> ReplayFiles { get; set; }
+
+        public string RLReplayPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\My Games\\Rocket League\\TAGame\\Demos";
+        public string ReplayFilePattern = "*.replay";
+
+        public ReplayDirectoryViewModel()
+        {
+            var children = DirectoryStructure.GetDirectoryFiles(RLReplayPath, ReplayFilePattern);
+
+            ReplayFiles = new ObservableCollection<DirectoryItemViewModel>(
+                children.Select(replay => new DirectoryItemViewModel(replay.FullPath, DirectoryItemType.File)));
+        }
+    }
+}
